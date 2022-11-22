49bb1ee58e0871df9903ccdf2754e4e3b3b05faf
Tesseract <tensai3355@gmail.com>
2022-11-22 17:21:47 +0900

    :lipstick: 폰트 변경

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -62,7 +62,7 @@ const GlobalStyle = createGlobalStyle`
   *::before,
   *::after {
     box-sizing: border-box;
-    font-family: 'Pretendard Variable', sans-serif;
+    font-family: 'SUIT Variable', sans-serif;
   }
 
   a {
