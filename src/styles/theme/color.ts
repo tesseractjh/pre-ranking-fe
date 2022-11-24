ab177ccaa583a8544aa40d3a2d8fa5e2a75ccb5b
Tesseract <tensai3355@gmail.com>
2022-11-25 02:46:23 +0900

    :pencil2: RED 색상 추가

--- src/styles/theme/color.ts
+++ src/styles/theme/color.ts
@@ -20,7 +20,17 @@ const color = {
   PURPLE_600: '#9333ea',
   PURPLE_700: '#7e22ce',
   PURPLE_800: '#6b21a8',
-  PURPLE_900: '#581c87'
+  PURPLE_900: '#581c87',
+  RED_50: '#fef2f2',
+  RED_100: '#fee2e2',
+  RED_200: '#fecaca',
+  RED_300: '#fca5a5',
+  RED_400: '#f87171',
+  RED_500: '#ef4444',
+  RED_600: '#dc2626',
+  RED_700: '#b91c1c',
+  RED_800: '#991b1b',
+  RED_900: '#7f1d1d'
 } as const;
 
 export default color;
