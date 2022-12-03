e10172ec600b0a202800c4ee6eed80a230bd542a
Tesseract <tensai3355@gmail.com>
2022-12-04 03:34:31 +0900

    :lipstick: yellow 색상 추가

--- src/styles/theme/color.ts
+++ src/styles/theme/color.ts
@@ -30,7 +30,17 @@ const color = {
   RED_600: '#dc2626',
   RED_700: '#b91c1c',
   RED_800: '#991b1b',
-  RED_900: '#7f1d1d'
+  RED_900: '#7f1d1d',
+  YELLOW_50: '#fefce8',
+  YELLOW_100: '#fef9c3',
+  YELLOW_200: '#fef08a',
+  YELLOW_300: '#fde047',
+  YELLOW_400: '#facc15',
+  YELLOW_500: '#eab308',
+  YELLOW_600: '#ca8a04',
+  YELLOW_700: '#a16207',
+  YELLOW_800: '#854d0e',
+  YELLOW_900: '#713f12'
 } as const;
 
 export default color;
