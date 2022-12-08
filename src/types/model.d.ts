5fae8a7f86f21be6dfe2a825479ee7647b695766
Tesseract <tensai3355@gmail.com>
2022-12-08 18:40:34 +0900

    :label: Prediction, StockFluctuation 타입 추가

--- src/types/model.d.ts
+++ src/types/model.d.ts
@@ -11,4 +11,26 @@ declare namespace Model {
     user_name: string;
     coin: number;
   }
+
+  interface Prediction {
+    prediction_id: number;
+    prediction_category: string;
+    prediction_info_id: number;
+    result_value: string;
+    result_date: string;
+    created_at: string;
+  }
+
+  interface StockFluctuation {
+    info_id: number;
+    stock_name: string;
+    last_price: number;
+    last_date: string;
+    code: string;
+    short_code: string;
+    market_category: string;
+    vs: number;
+    fluctuation_rate: number;
+    created_at: string;
+  }
 }
