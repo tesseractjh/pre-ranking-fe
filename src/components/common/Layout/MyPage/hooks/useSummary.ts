0474dbc5be585685fe74182e18c6f2bea51657ef
Tesseract <tensai3355@gmail.com>
2022-12-16 04:46:03 +0900

    :bug: 마이페이지 개요 적중률이 제대로 표시되지 않는 버그 수정

--- src/components/common/Layout/MyPage/hooks/useSummary.ts
+++ src/components/common/Layout/MyPage/hooks/useSummary.ts
@@ -6,7 +6,7 @@ import useUserInfo from '@hooks/queries/useUserInfo';
 function useSummary() {
   const { data: userInfoData } = useUserInfo(true);
   const { data: myRankData } = useMyRank();
-  const { data: predictionCountData } = usePredictionCount();
+  const { data: predictionCountData } = usePredictionCount('all');
 
   const nextLevel = LEVEL.findIndex(
     (exp) => userInfoData?.user && userInfoData.user?.exp < exp
