87f0efdc5e8e9b06fe60a29ec37c45649a6b49b1
Tesseract <tensai3355@gmail.com>
2022-12-21 00:52:40 +0900

    :sparkles: open graph 메타 태그, favicon.ico, robots.txt 추가

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import * as path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
+  assetsInclude: ['public/*'],
   server: {
     open: true,
     port: 3000
